e8343a9958caef2d32d720801c0f30c78d5e1ed5
katherineanthony <katherinesanthony@gmail.com>
2019-09-16 13:58:41 -0700

    Initial commit

---
+++ app/src/main/java/com/example/SoundBoardActivity/SoundsActivity.java
@@ -0,0 +1,14 @@
+package com.example.SoundBoardActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoundsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sounds);
+    }
+}
