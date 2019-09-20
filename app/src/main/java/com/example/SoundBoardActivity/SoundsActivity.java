27167684163e6301f897c44286cd0cb46d152867
katherineanthony <katherinesanthony@gmail.com>
2019-09-20 12:49:59 -0700

    we did the stuff today. yeeted w three keys (put them in da layout) and then we added all the notes!

--- app/src/main/java/com/example/SoundBoardActivity/SoundsActivity.java
+++ app/src/main/java/com/example/SoundBoardActivity/SoundsActivity.java
@@ -2,13 +2,123 @@ package com.example.SoundBoardActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.media.SoundPool.OnLoadCompleteListener;
+import android.widget.ViewAnimator;
+
 
 public class SoundsActivity extends AppCompatActivity {
+    private Button buttonA;
+    private Button buttonBflat;
+    private Button buttonB;
+    private int aNote;
+    private int bNote;
+    private int bflatNote;
+    private SoundPool soundPool;
+    private int soundA;
+    private int soundB;
+    private int soundBb;
+    private boolean loaded;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sounds);
+        wireWidgets();
+        //this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        loaded = false;
+
+        //wirewidgets();
+
+        loadSoundPool();
+        liamsuckssetListeners();
+
+
     }
+
+    private void loadSoundPool() {
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        aNote = soundPool.load(this, R.raw.scalea,1);
+        bNote = soundPool.load(this, R.raw.scaleb,1);
+        bflatNote = soundPool.load(this, R.raw.scalebb,1);
+        loaded = true;
+    }
+
+
+    private void liamsuckssetListeners() {
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+                    @Override
+                    public void onLoadComplete(SoundPool soundPool, int sampleId,
+                                               int status) {
+                        loaded = true;
+                    }
+                });
+                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                float actualVolume = (float) audioManager
+                        .getStreamVolume(AudioManager.STREAM_MUSIC);
+                float maxVolume = (float) audioManager
+                        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                float volume = actualVolume / maxVolume;
+                if(loaded)
+                    soundPool.play(aNote, volume, volume, 1, 0, 1f);
+            }
+        });
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+                    @Override
+                    public void onLoadComplete(SoundPool soundPool, int sampleId,
+                                               int status) {
+                        loaded = true;
+                    }
+                });
+                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                float actualVolume = (float) audioManager
+                        .getStreamVolume(AudioManager.STREAM_MUSIC);
+                float maxVolume = (float) audioManager
+                        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                float volume = actualVolume / maxVolume;
+                if(loaded)
+                    soundPool.play(bNote, volume, volume, 1, 0, 1f);
+            }
+        });
+        buttonBflat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+                    @Override
+                    public void onLoadComplete(SoundPool soundPool, int sampleId,
+                                               int status) {
+                        loaded = true;
+                    }
+                });
+                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                float actualVolume = (float) audioManager
+                        .getStreamVolume(AudioManager.STREAM_MUSIC);
+                float maxVolume = (float) audioManager
+                        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                float volume = actualVolume / maxVolume;
+                if(loaded)
+                    soundPool.play(bflatNote, volume, volume, 1, 0, 1f);
+            }
+        });
+    }
+
+    private void wireWidgets() {
+        buttonA=findViewById(R.id.button_main_a);
+        buttonB=findViewById(R.id.button_main_b);
+        buttonBflat=findViewById(R.id.button_main_bflat);
+    }
+
 }
